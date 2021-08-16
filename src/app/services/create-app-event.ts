bfb7c323e4e066fc06658aa83a83f965ec9e4a6e
Jose Guillermo <jguillermo@outlook.com>
2021-08-16 18:53:30 -0500

    feat(event): render event application

--- src/app/services/create-app-event.ts
+++ src/app/services/create-app-event.ts
@@ -17,25 +17,24 @@ export class Service extends AbstractService {
   async execute(aggregateName: string): Promise<void> {
     const listEvents = this.listEvent();
     if (listEvents.length === 0) {
-      console.log('No exsit Events');
+      console.log('No exist Events');
       return;
     }
 
-    const answerEventName = await inquirer.prompt(Service.questionEventName());
-
     const answerEventList = await inquirer.prompt(Service.questionEventList(listEvents));
 
-    const answerClassName = await inquirer.prompt(Service.questionClassName(answerEventName.eventName, answerEventList.eventSelected));
+    const answerEventName = await inquirer.prompt(Service.questionName());
 
     const render = new ServiceRender(this._collectionAggregate, this.language);
+    await render.execute(aggregateName, { eventSelected: answerEventList.eventSelected, eventName: answerEventName.eventName });
   }
 
-  private static questionEventName(): QuestionCollection<{ eventName: string }> {
+  private static questionName(): QuestionCollection<{ eventName: string }> {
     return [
       {
         type: 'input',
         name: 'eventName',
-        message: `Event name`,
+        message: `Name`,
         default: 'Resource',
         validate(input: any): boolean | string | Promise<boolean | string> {
           if (s.trim(input).length < 3) {
@@ -71,108 +70,34 @@ export class Service extends AbstractService {
       },
     ];
   }
-
-  private static questionClassName(eventName: string, eventSelected: string): QuestionCollection<{ className: string }> {
-    const tempEventSelected = `${eventSelected}`.replace('Event', '');
-    return [
-      {
-        type: 'input',
-        name: 'commandName',
-        message: `COMMAND name`,
-        default: `${eventName}On${tempEventSelected}`,
-      },
-    ];
-  }
 }
 
 export class ServiceRender extends AbstractServiceResponse {
   get templatePath(): string {
-    return `${this.language.language()}/application/command/`;
+    return `${this.language.language()}/infrastructure/event`;
   }
 
-  async execute(aggregateName: string, options: { properties: string[]; commandName: string; templateRender: string }): Promise<void> {
+  async execute(aggregateName: string, options: { eventSelected: string; eventName: string }): Promise<void> {
     const aggregate = this._collectionAggregate.getAggregate(aggregateName);
-    const propertiesSelected = storage.getWProperties(options.properties).map((e) => {
-      e.setLanguage(this.language);
-      return e;
-    });
 
-    this.renderDto(aggregate, propertiesSelected, options.commandName, options.templateRender);
-
-    this.renderHandler(aggregate, propertiesSelected, options.commandName);
-
-    this.renderService(aggregate, propertiesSelected, options.commandName, options.templateRender);
+    this.renderDto(aggregate, options.eventSelected, options.eventName);
   }
 
-  private renderDto(aggregate: Aggregate, properties: WPropertie[], commandName: string, templateRender: string) {
-    const className = this.language.className([aggregate.name.value, commandName, 'Dto']);
-    const generateFile = this.language.classFile([aggregate.name.value, commandName, 'Dto']);
-    const generatefolder = this.language.folderPath([aggregate.path.value, 'application', commandName]);
+  private renderDto(aggregate: Aggregate, eventSelected: string, eventName: string) {
+    const tempEventSelected = `${eventSelected}`.replace('Event', '');
+
+    const eventFile = this.language.classFile([tempEventSelected, 'event']);
+
+    const className = this.language.className([eventName, 'on', tempEventSelected]);
+    const generateFile = this.language.classFileWithOutType([eventName, 'on', tempEventSelected]);
+    const generatefolder = this.language.folderPath([aggregate.path.value, 'infrastructure', 'event']);
 
     Render.generate({
-      templateFile: `${this.templatePath}dto.ejs`,
+      templateFile: `${this.templatePath}/event.ejs`,
       templateData: {
         className,
-        properties,
-        templateRender,
-      },
-      generatefolder,
-      generateFile,
-    });
-  }
-
-  private renderHandler(aggregate: Aggregate, properties: WPropertie[], commandName: string) {
-    const classDto = this.language.className([aggregate.name.value, commandName, 'Dto']);
-    const fileDto = this.language.classFile([aggregate.name.value, commandName, 'Dto'], false);
-
-    const classService = this.language.className([aggregate.name.value, commandName, 'service']);
-    const fileService = this.language.classFile([aggregate.name.value, commandName, 'service'], false);
-
-    const className = this.language.className([aggregate.name.value, commandName, 'handler']);
-    const generateFile = this.language.classFile([aggregate.name.value, commandName, 'handler']);
-    const generatefolder = this.language.folderPath([aggregate.path.value, 'application', commandName]);
-
-    Render.generate({
-      templateFile: `${this.templatePath}handler.ejs`,
-      templateData: {
-        classDto,
-        fileDto,
-        classService,
-        fileService,
-        className,
-        properties,
-        strProperties: properties.map((e) => e.propertie.name.value).join(', '),
-      },
-      generatefolder,
-      generateFile,
-    });
-  }
-
-  private renderService(aggregate: Aggregate, properties: WPropertie[], commandName: string, templateRender: string) {
-    const classRepository = this.language.className([aggregate.name.value, 'Repository']);
-    const fileRepository = this.language.classFile([aggregate.name.value, 'Repository'], false);
-
-    const fileAggregate = this.language.classFileWithOutType([aggregate.name.value], false);
-
-    const className = this.language.className([aggregate.name.value, commandName, 'service']);
-    const generateFile = this.language.classFile([aggregate.name.value, commandName, 'service']);
-    const generatefolder = this.language.folderPath([aggregate.path.value, 'application', commandName]);
-
-    const propertiesWithoutId = properties.filter((e) => e.propertie.name.value !== 'id');
-
-    Render.generate({
-      templateFile: `${this.templatePath}service.ejs`,
-      templateData: {
-        classRepository,
-        fileRepository,
-        templateRender,
-        className,
-        aggregate,
-        fileAggregate,
-        properties,
-        strProperties: properties.map((e) => e.propertie.name.value).join(', '),
-        strPropertiesWitoutId: propertiesWithoutId.map((e) => e.propertie.name.value).join(', '),
-        strVoProperties: properties.map((e) => `${e.propertie.name.value}: ${e.propertie.className}`).join(', '),
+        eventSelected,
+        eventFile,
       },
       generatefolder,
       generateFile,
