3deea3544e5d7242fa70e0bfe609538d25589b02
Jose Guillermo <jguillermo@outlook.com>
2021-08-19 21:48:38 -0500

    feat(menu): add menu event index generator

--- src/app/services/menu-app/index.ts
+++ src/app/services/menu-app/index.ts
@@ -1,3 +1,4 @@
 import { ServiceMenuFirestore } from './service-menu-firestore';
+import { ServiceMenuEventIndex } from './service-menu-event-index';
 
-export const menuServices = [new ServiceMenuFirestore()];
+export const menuServices = [new ServiceMenuFirestore(), new ServiceMenuEventIndex()];
