0b6246d983a450545676d327fd4661e1683c632a
Jose Guillermo <jguillermo@outlook.com>
2021-08-21 23:31:13 -0500

    refactor(menu): order menu

--- src/app/menu/dynamic-list-2-app.ts
+++
@@ -1,5 +0,0 @@
-import { ServiceMenuEventIndex } from '../services/infrastructure/service-menu-event-index';
-import { ServiceMenuApplicationIndex } from '../services/application/service-menu-application-index';
-import { ServiceMenuInitProject } from '../services/app/service-menu-init-project';
-
-export const menuServices = [new ServiceMenuInitProject(), new ServiceMenuEventIndex(), new ServiceMenuApplicationIndex()];
--- src/app/menu/dynamic-list-2-services.ts
+++ src/app/menu/dynamic-list-2-services.ts
@@ -3,5 +3,15 @@ import { ServiceMenuPropertie } from '../services/domain/service-menu-propertie'
 import { ServiceMenuRepository } from '../services/infrastructure/service-menu-repository';
 import { ServiceMenuAggregate } from '../services/domain/service-menu-aggregate';
 import { ServiceMenuResponse } from '../services/application/service-menu-response';
+import { ServiceMenuEventIndex } from '../services/infrastructure/service-menu-event-index';
+import { ServiceMenuApplicationIndex } from '../services/application/service-menu-application-index';
 
-export const menuServices = [new ServiceMenuGraphQl(), new ServiceMenuPropertie(), new ServiceMenuRepository(), new ServiceMenuAggregate(), new ServiceMenuResponse()];
+export const menuServices = [
+  new ServiceMenuGraphQl(),
+  new ServiceMenuPropertie(),
+  new ServiceMenuRepository(),
+  new ServiceMenuAggregate(),
+  new ServiceMenuResponse(),
+  new ServiceMenuEventIndex(),
+  new ServiceMenuApplicationIndex(),
+];
