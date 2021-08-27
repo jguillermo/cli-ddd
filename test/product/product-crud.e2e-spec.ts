3a7fe923b0d85419fe352969a394f61206991e45
Jose Guillermo <jguillermo@outlook.com>
2021-08-27 15:23:46 -0500

    refactor(pretier): add pretier in render file

--- test/product/product-crud.e2e-spec.ts
+++ test/product/product-crud.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { cleanRender, DDD, menu, MenuPropertie, readRender, readSnapShot, run } from '../load-cmd';
 
-// const PATH_PRODUCT = 'src/product';
-// const PATH_PRODUCT_DOMAIN = PATH_PRODUCT + '/domain';
-// const PATH_PRODUCT_APPLICATION = PATH_PRODUCT + '/application';
-// const PATH_PRODUCT_INFRASTRUCTURE = PATH_PRODUCT + '/infrastructure';
+const RENDER_PATH = 'src/product';
+const RENDER_PATH_DOMAIN = RENDER_PATH + '/domain';
+// const RENDER_PATH_APPLICATION = RENDER_PATH + '/application';
+// const RENDER_PATH_INFRASTRUCTURE = RENDER_PATH + '/infrastructure';
 //
-// const SNAP_PATH_PRODUCT = '/user';
-// const SNAP_PATH_PRODUCT_DOMAIN = '/user/domain';
-// const SNAP_PATH_PRODUCT_APPLICATION = '/user/application';
-// const SNAP_PATH_PRODUCT_INFRASTRUCTURE = '/user/infrastructure';
+const SNAP_PATH = '/product';
+const SNAP_PATH_DOMAIN = SNAP_PATH + '/domain';
+// const SNAP_PATH_APPLICATION = '/user/application';
+// const SNAP_PATH_INFRASTRUCTURE = '/user/infrastructure';
 
 const PATH_PRODUCT_TEST = 'test/product';
 const SNAP_PATH_PRODUCT_TEST = '/product/test';
@@ -19,6 +19,51 @@ describe('Product crud', () => {
     cleanRender();
     await run(MENU);
   });
+  describe('domain', () => {
+    test('properties', async () => {
+      console.log(RENDER_PATH_DOMAIN + '/product-category.ts');
+      const renderCategory = readRender(RENDER_PATH_DOMAIN + '/product-category.ts');
+      // const renderCode = readRender(RENDER_PATH_DOMAIN + '/product-code.ts');
+      // const renderCreateAt = readRender(RENDER_PATH_DOMAIN + '/product-create-at.ts');
+      // const renderDescription = readRender(RENDER_PATH_DOMAIN + '/product-description.ts');
+      // const renderId = readRender(RENDER_PATH_DOMAIN + '/product-id.ts');
+      // const renderName = readRender(RENDER_PATH_DOMAIN + '/product-name.ts');
+      // const renderPrice = readRender(RENDER_PATH_DOMAIN + '/product-price.ts');
+
+      const snapRenderCategory = readSnapShot(SNAP_PATH_DOMAIN + '/product-category.txt');
+      // const snapRenderCode = readSnapShot(SNAP_PATH_DOMAIN + '/product-code.txt');
+      // const snapRenderCreateAt = readSnapShot(SNAP_PATH_DOMAIN + '/product-create-at.txt');
+      // const snapRenderDescription = readSnapShot(SNAP_PATH_DOMAIN + '/product-description.txt');
+      // const snapRenderId = readSnapShot(SNAP_PATH_DOMAIN + '/product-id.txt');
+      // const snapRenderName = readSnapShot(SNAP_PATH_DOMAIN + '/product-name.txt');
+      // const snapRenderPrice = readSnapShot(SNAP_PATH_DOMAIN + '/product-price.txt');
+
+      expect(renderCategory).toEqual(snapRenderCategory);
+      // expect(renderCode).toEqual(snapRenderCode);
+      // expect(renderCreateAt).toEqual(snapRenderCreateAt);
+      // expect(renderDescription).toEqual(snapRenderDescription);
+      // expect(renderId).toEqual(snapRenderId);
+      // expect(renderName).toEqual(snapRenderName);
+      // expect(renderPrice).toEqual(snapRenderPrice);
+    });
+    test.only('aggregate', async () => {
+      const renderAggregate = readRender(RENDER_PATH_DOMAIN + '/product.ts');
+      // const renderEventCreated = readRender(RENDER_PATH_DOMAIN + '/product-created.event.ts');
+      // const renderEventUpdated = readRender(RENDER_PATH_DOMAIN + '/product-updated.event.ts');
+      // const renderEventDeleted = readRender(RENDER_PATH_DOMAIN + '/product-deleted.event.ts');
+
+      const snapAggregate = readSnapShot(SNAP_PATH_DOMAIN + '/product.txt');
+      // const snapEventCreated = readSnapShot(SNAP_PATH_DOMAIN + '/product-created.event.txt');
+      // const snapEventUpdated = readSnapShot(SNAP_PATH_DOMAIN + '/product-updated.event.txt');
+      // const snapEventDeleted = readSnapShot(SNAP_PATH_DOMAIN + '/product-deleted.event.txt');
+
+      expect(renderAggregate).toEqual(snapAggregate);
+      // expect(renderEventCreated).toEqual(snapEventCreated);
+      // expect(renderEventUpdated).toEqual(snapEventUpdated);
+      // expect(renderEventDeleted).toEqual(snapEventDeleted);
+    });
+  });
+
   describe('test e2e', () => {
     test('delete', async () => {
       await run([...MENU]);
